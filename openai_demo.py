a8d225710ea0767291c4f82228a169599400b090
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-10-01 20:51:37 +0000

    Add `openai_demo.py` to demonstrate basic OpenAI GPT-4o integration.

---
+++ openai_demo.py
@@ -0,0 +1,15 @@
+import os
+
+from langchain_openai import ChatOpenAI
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+llm = ChatOpenAI(model="gpt-4o", api_key=OPENAI_API_KEY)
+
+question = input("Enter the question: ")
+response = llm.invoke(question)
+print(response)
